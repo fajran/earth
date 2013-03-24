9dd2d4cd3dd1fb9c2fae7248856e1d6b04363bff
Fajran Iman Rusadi <fajran@gmail.com>
2013-03-24 18:53:47 +0100

    Add shader loader

---
+++ src/shader.cc
@@ -0,0 +1,96 @@
+#include "shader.h"
+
+#include <assert.h>
+#include <fstream>
+#include <streambuf>
+#include <iostream>
+
+namespace e {
+
+static std::string LoadFile(const std::string fname) {
+  std::ifstream s(fname.c_str());
+  std::string content((std::istreambuf_iterator<char>(s)),
+                      std::istreambuf_iterator<char>());
+  return content;
+}
+
+Shader::Shader(const std::string vertex_shader_file,
+               const std::string fragment_shader_file)
+    : vertex_shader_file_(vertex_shader_file),
+      fragment_shader_file_(fragment_shader_file),
+      program_(0) {
+}
+
+Shader::~Shader() {
+  Unload();
+}
+
+void Shader::Load() {
+  std::string vs = LoadFile(vertex_shader_file_);
+  std::string fs = LoadFile(fragment_shader_file_);
+
+  char const* src;
+  GLint res = GL_FALSE;
+
+  // compile and check vertex shader
+  src = vs.c_str();
+  GLuint vsId = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vsId, 1, &src, NULL);
+  glCompileShader(vsId);
+
+  glGetShaderiv(vsId, GL_COMPILE_STATUS, &res);
+  if (res != GL_TRUE) {
+    int length;
+    glGetShaderiv(vsId, GL_INFO_LOG_LENGTH, &length);
+    char msg[length+1];
+    glGetShaderInfoLog(vsId, length, NULL, msg);
+    std::cerr << "Vertex shader error: " << msg << std::endl;
+  }
+  assert(res == GL_TRUE);
+
+  // compile and check fragment shader
+  src = fs.c_str();
+  GLuint fsId = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fsId, 1, &src, NULL);
+  glCompileShader(fsId);
+
+  glGetShaderiv(fsId, GL_COMPILE_STATUS, &res);
+  if (res != GL_TRUE) {
+    int length;
+    glGetShaderiv(fsId, GL_INFO_LOG_LENGTH, &length);
+    char msg[length+1];
+    glGetShaderInfoLog(fsId, length, NULL, msg);
+    std::cerr << "Fragment shader error: " << msg << std::endl;
+  }
+  assert(res == GL_TRUE);
+
+  // create program
+  GLuint programId = glCreateProgram();
+  glAttachShader(programId, vsId);
+  glAttachShader(programId, fsId);
+  glLinkProgram(programId);
+
+  glGetProgramiv(programId, GL_LINK_STATUS, &res);
+  if (res != GL_TRUE) {
+    int length;
+    glGetProgramiv(programId, GL_INFO_LOG_LENGTH, &length);
+    char msg[length+1];
+    glGetProgramInfoLog(programId, length, NULL, msg);
+    std::cerr << "Program link error! " << msg << std::endl;
+  }
+  assert(res == GL_TRUE);
+
+  glDeleteShader(vsId);
+  glDeleteShader(fsId);
+
+  program_ = programId;
+}
+
+void Shader::Unload() {
+  if (program_ != 0) {
+    glDeleteProgram(program_);
+  }
+}
+
+}
+
---
+++ src/shader.h
@@ -0,0 +1,31 @@
+#ifndef EARTH_SHADER_H_
+#define EARTH_SHADER_H_
+
+#include <string>
+
+#include <GL/glew.h>
+
+namespace e {
+
+class Shader {
+ public:
+  Shader(const std::string vertex_shader_file,
+         const std::string fragment_shader_file);
+  virtual ~Shader();
+
+  void Load();
+  void Unload();
+
+  GLuint program() { return program_; }
+
+ private:
+  const std::string vertex_shader_file_;
+  const std::string fragment_shader_file_;
+
+  GLuint program_;
+};
+
+}
+
+#endif
+
