eaed2101a57b45456091b478cf472c4863131961
Fajran Iman Rusadi <fajran@gmail.com>
2013-03-25 04:37:17 +0100

    Change MVP matrix

--- src/earth.cc
+++ src/earth.cc
@@ -2,6 +2,7 @@
 
 #include <GL/glew.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include "shader.h"
 
@@ -51,7 +52,16 @@ void Earth::Init() {
   posId = shader->GetAttribLocation("pos");
   mvpId = shader->GetUniformLocation("mvp");
 
-  mvp = glm::mat4(1.0f);
+  {
+    glm::mat4 projection = glm::perspective(45.0f, 4.0f/3.0f, 0.1f, 100.0f);
+    glm::mat4 view = glm::lookAt(
+      glm::vec3(4, 3, 3),
+      glm::vec3(0, 0, 0),
+      glm::vec3(0, 1, 0)
+    );
+    glm::mat4 model = glm::mat4(1.0f);
+    mvp = projection * view * model;
+  }
 }
 
 void Earth::Update() {
