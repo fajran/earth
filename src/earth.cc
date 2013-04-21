ceb5f9f6c1abd5951d66ca757da5b6fc1b6a8198
Fajran Iman Rusadi <fajran@gmail.com>
2013-04-21 15:06:16 +0200

    Enable blending to show transparency

--- src/earth.cc
+++ src/earth.cc
@@ -59,6 +59,9 @@ void Earth::Render() {
   glEnable(GL_DEPTH_TEST);
   glDepthFunc(GL_LESS);
 
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
   tile->Draw(camera.Matrix());
 }
 
